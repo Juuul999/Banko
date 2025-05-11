34615723cbda1e2a0696f18c416fd747c8efe86e
EDMerstratec <erme0001@edu.mercantec.dk>
2025-05-11 15:16:28 +0200

    mvp functions

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -2,57 +2,106 @@
 {
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
 
-            //current test tables
-            BankoTable table1 = new BankoTable()
+            //static test tables
+            Dictionary<int, int[]> myTables = new Dictionary<int, int[]>();
+            //test name Noah
+            //myTables[1,1] accesses table 1, value 1, i.e. 10
+            myTables.Add(1, [1, 10, 23, 45, 70, 2, 24, 35, 67, 86, 19, 57, 68, 76, 90]);
+            myTables.Add(2, [14, 21, 32, 76, 86, 6, 15, 44, 56, 66, 29, 45, 58, 79, 88]);
+            myTables.Add(3, [1, 20, 64, 70, 81, 11, 48, 54, 73, 82, 9, 15, 37, 59, 67]);
+            myTables.Add(4, [1, 30, 42, 61, 70, 24, 45, 55, 64, 85, 15, 25, 34, 47, 69]);
+            myTables.Add(5, [3, 20, 31, 52, 60, 6, 15, 21, 46, 86, 18, 25, 33, 79, 88]);
+            Console.WriteLine("qq to quit, reset to reset, number between 1 and 90 to continue");
+
+            string bankoInput = "";
+            List<int> calledNums = new List<int>();
+            //Looping input until quitting; add to list calledNums if valid, then loop over tables
+            do
             {
-                tableNums = [11, 31, 41, 61, 81, 12, 32, 42, 62, 82, 13, 33, 43, 63, 83],
-                tableID = "testTable1"
-            };
-            BankoTable table2 = new BankoTable()
-            {
-                tableNums = [14, 34, 44, 64, 84, 15, 35, 45, 65, 85, 16, 36, 46, 66, 86],
-                tableID = "testTable2"
-            };
-            Console.WriteLine($"Hello to {table2.tableID}");
-
-            //Variable containing called numbers
-            int[] calledNums = [];
-            calledNums = [11, 17, 45, 16];
-
-            CheckNumbs(table1.tableNums, calledNums);
-            CheckNumbs(table2.tableNums, calledNums);
-
-            //function to loop over table's numbers, comparing to called numbers
-            static void CheckNumbs(int[] thisTableNums, int[] calledNums)
-            {
-
-                // Array.ForEach(thisTableNums, Console.WriteLine);
-                // Console.WriteLine();
-                //Array.ForEach(calledNums, Console.WriteLine);
-
-                foreach (int num in calledNums)
+                bankoInput = Console.ReadLine();
+                switch (bankoInput)
                 {
-                    if (thisTableNums.Contains(num))
-                    {
-                        Console.WriteLine(num + "check");
-                    }
+                    case "qq":
+                        //quit
+                        Console.WriteLine("Quitting");
+                        break;
+                    case "reset":
+                        Console.WriteLine("Resetti");
+                        //reset
+                        break;
+                    default:
+                        if (int.TryParse(bankoInput, out int bankoNum) && (0 < bankoNum) && (91 > bankoNum) && (calledNums.Contains(bankoNum) != true))
+                        {
+                            //Console.WriteLine("Valid Num");
+                            calledNums.Add(bankoNum);
+                            Console.Write("Currently called numbers: ");
+                            foreach (int number in calledNums) 
+                            { 
+                                Console.Write($"{number}, ");
+                            }
+                            Console.WriteLine();
+                            //Console.WriteLine(myTables.Count);
+                            for (int iTable = 1; iTable <= myTables.Count; iTable++)
+                            {
+                                checkTable(iTable);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. qq, reset, or unique number between 1 and 90");
+                        }
+                        //err
+                        break;
                 }
             }
+            while (bankoInput != "qq");
 
+            void checkTable(int tableID)
+            {
+                int[] bingoBankoCounter = [0, 0, 0];
+                for (int row = 1; row < 4; row++)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (calledNums.Contains(myTables[tableID][i+5*(row-1)]))
+                        {
+                            bingoBankoCounter[row - 1]++;
+                        }
 
+                    }
+                }
+                if (bingoBankoCounter.Sum() == 15)
+                {
+                    Console.WriteLine($"Banko on all rows!");
+                }
+                else if (bingoBankoCounter.Contains(5))
+                {
+                    for (int i = 0; i<3; i++) 
+                    {
+                        if (bingoBankoCounter[i] == 5)
+                        {
+                            Console.WriteLine($"Banko on row {i+1}!");
+                        }
+                    }
+                }
+                else
+                { Console.WriteLine($"row1: {bingoBankoCounter[0]}, row2: {bingoBankoCounter[1]}, row3: {bingoBankoCounter[2]}");
+                }
+                //Console.WriteLine($"Bingo-Banko on {row}");
+            }
         }
+/*myTables.Add(1, [1, 10, 23, 45, 70, 2, 24, 35, 67, 86, 19, 57, 68, 76, 90]);
+            myTables.Add(2, [14, 21, 32, 76, 86, 6, 15, 44, 56, 66, 29, 45, 58, 79, 88]);
+            myTables.Add(3, [1, 20, 64, 70, 81, 11, 48, 54, 73, 82, 9, 15, 37, 59, 67]);
+            myTables.Add(4, [1, 30, 42, 61, 70, 24, 45, 55, 64, 85, 15, 25, 34, 47, 69]);
+            myTables.Add(5, [3, 20, 31, 52, 60, 6, 15, 21, 46, 86, 18, 25, 33, 79, 88]);
+        */    
+        
+
     }
-
-
-
-    public class BankoTable
-    {
-        public int[]? tableNums;
-        public string tableID;
-    }
-
-
 }
